0211ac3f45453a30581088033d6e12bc63ee8144
JoyfulSphericalCreature <oliviachrisley5@gmail.com>
2024-11-02 16:28:26 -0400

    Add Today.java

---
+++ Today.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+class Today {
+  public static void main(String args []) {
+    LocalDate todaysDate = LocalDate.now();
+    System.out.println("Today is " + todaysDate);
+  }
+}
